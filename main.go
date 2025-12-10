a182643282be16c13c1d62a1cb0077b60ddee035
MagnusTrier <magnustrier@gmail.com>
2025-12-11 00:03:47 +0100

    finished chapter 3

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -33,6 +35,70 @@ func (cfg *apiConfig) handlerResetFileserverHits(w http.ResponseWriter, r *http.
 	cfg.fileserverHits.Store(0)
 }
 
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type requestVals struct {
+		Body string `json:"body"`
+	}
+
+	type returnValsError struct {
+		Msg string `json:"error"`
+	}
+
+	defer r.Body.Close()
+
+	decoder := json.NewDecoder(r.Body)
+	params := requestVals{}
+	if err := decoder.Decode(&params); err != nil {
+		w.WriteHeader(500)
+
+		data, err := json.Marshal(returnValsError{Msg: fmt.Sprintf("Error decoding parameters: %s", err)})
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		w.Write(data)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		w.WriteHeader(400)
+		data, err := json.Marshal(returnValsError{Msg: "chirp is too long"})
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Print(err)
+			return
+		}
+		w.Write(data)
+		return
+	}
+
+	notAllowed := []string{"kerfuffle", "sharbert", "fornax", "Kerfuffle", "Sharbert", "Fornax"}
+	cleanedChirp := params.Body
+
+	for _, w := range notAllowed {
+		cleanedChirp = strings.ReplaceAll(cleanedChirp, w, "****")
+	}
+
+	type returnValsSuccess struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	resBody := returnValsSuccess{
+		CleanedBody: cleanedChirp,
+	}
+
+	data, err := json.Marshal(resBody)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Printf("Error marshalling JSON: %s", err)
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write(data)
+}
+
 func main() {
 
 	mux := http.NewServeMux()
@@ -54,6 +120,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", customHandler)
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerFileserverHits)
 	mux.HandleFunc("POST /admin/reset", cfg.handlerResetFileserverHits)
+
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	s := http.Server{
 		Handler: mux,
