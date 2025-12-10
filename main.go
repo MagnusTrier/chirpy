607219e91f0444ffddb8cccd65ceb56aa033d836
MagnusTrier <magnustrier@gmail.com>
2025-12-10 23:10:30 +0100

    finished chapter 2

--- main.go
+++ main.go
@@ -1,30 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerFileserverHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	n := cfg.fileserverHits.Load()
+	body := fmt.Sprintf("Hits: %v", n)
+
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) handlerResetFileserverHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 
-	sm := http.NewServeMux()
+	mux := http.NewServeMux()
+
+	cfg := apiConfig{}
 
 	filepath := http.Dir(".")
 
 	customHandler := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
-
 	}
 
-	// sm.Handle("/app/assets/logo.png", http.StripPrefix("/app/", http.FileServer(filepath)))
-	sm.Handle("/app/", http.StripPrefix("/app/", http.FileServer(filepath)))
+	handler := http.StripPrefix("/app/", http.FileServer(filepath))
 
-	sm.HandleFunc("/healthz", customHandler)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
+
+	mux.HandleFunc("GET /healthz", customHandler)
+	mux.HandleFunc("GET /metrics", cfg.handlerFileserverHits)
+	mux.HandleFunc("POST /reset", cfg.handlerResetFileserverHits)
 
 	s := http.Server{
-		Handler: sm,
+		Handler: mux,
 		Addr:    ":8080",
 	}
 
